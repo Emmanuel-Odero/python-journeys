e7c7a8199fd352cd934f856a419593b0a9a4b1e1
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-06-04 21:29:14 +0300

    done with making the application read and write to db yet to work on the update

--- projectFlasks/todoApp/app.py
+++ projectFlasks/todoApp/app.py
@@ -1,6 +1,6 @@
-import imp
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://user1:password@localhost/sampledb'
@@ -16,6 +16,38 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 @app.route('/')
 def index():
