01b898da3675353e3995416b68eaa310136895ba
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-10-17 10:43:46 +0000

    assignment and mathematical operators

---
+++ operators.py
@@ -0,0 +1,18 @@
+#Assignment Operators use the equal(=) sign
+#Assignment operators are used for assignment
+first_name = "Otieno"
+sir_name = "Jaoko"
+
+#Comparison operataors are used for comparing variables
+#Comparison Operator use the ==
+#Logical Operators are used for logic inside our program
+location_1 = "East Legon"
+location_2 = "West Legon"
+
+a = "name"
+a +="1"
+print(a)
+
+if location_1 !=location_2:
+    print("The locations are not the same")
+print(f"Mr. {sir_name} {first_name} does not reside at either {location_1}{ location_2}")
