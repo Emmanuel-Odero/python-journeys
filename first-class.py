39441f3b0004089c2a47ad129cd45568297f52c8
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-11-01 05:46:48 +0000

    created my first class

---
+++ first-class.py
@@ -0,0 +1,18 @@
+class Employee:
+    pass
+
+employee_1 = Employee()
+employee_2 = Employee()
+
+employee_1.first_name = "Mercy"
+employee_1.last_name = "Masika"
+employee_1.salary = 33338
+employee_1.email = "mercy.masica@email.com"
+
+employee_2.first_name = "John"
+employee_2.last_name = "Doe"
+employee_2.salary = 85858
+employee_2.email = "john.doe@email.com"
+
+print(employee_1.email)
+print(employee_2.email)
